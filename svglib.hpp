1e84ad5e0456b4f4dad7ffd2704324592a66bd31
paulkazusek <paul.kazusek@gmail.com>
2023-08-10 22:18:05 +0200

    add point class

--- svglib.hpp
+++ svglib.hpp
@@ -72,6 +72,37 @@ namespace svglib
         double width_;
         double height_;
     };
+
+    class Point
+    {
+    public:
+        constexpr Point() = delete;
+
+        constexpr Point( const double& x, const double& y )
+            : x_ { x }, y_ { y }
+        {}
+
+        constexpr ~Point() = default;
+
+        [[nodiscard]] constexpr auto x() const
+        {
+            return x_;
+        }
+
+        [[nodiscard]] constexpr auto y() const
+        {
+            return y_;
+        }
+
+        constexpr auto operator==( const Point& other ) const -> bool
+        {
+			return x() == other.x() and y() == other.y();
+        }
+
+    private:
+        double x_;
+        double y_;
+    };
 }
 
 #endif
