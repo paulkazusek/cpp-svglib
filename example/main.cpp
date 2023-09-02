6d9cbae31f51fb4d1fca2ef88ae3311863409128
paulkazusek <paul.kazusek@gmail.com>
2023-09-02 20:19:27 +0200

    change project structure to headeronly

--- example/main.cpp
+++ example/main.cpp
@@ -10,12 +10,13 @@ auto main() -> int
     using svglib::ColorName;
 
     //constexpr auto color = svglib::Color( 211, 211, 211 );
-    constexpr auto color = Color( ColorName::alice_blue );
-    std::cout << std::format( "rgb( {}, {}, {} )", color.red(), color.green(), color.blue() ) << std::endl;
+    constexpr auto alice_blue = Color( ColorName::alice_blue );
+    std::cout << std::format( "rgb( {}, {}, {} )", alice_blue.red(), alice_blue.green(), alice_blue.blue() ) << std::endl;
 
-    //constexpr auto color = from_hex( "0077FF" );
-    //const  auto hex = to_hex( color );
-    //std::cout << hex << std::endl;
+    using svglib::from_hex;
+    constexpr auto color = from_hex( "0077FF" );
+    const  auto hex = to_hex( color );
+    std::cout << hex << std::endl;
 
     //std::cout << std::format( "color=\"{}\"", color) << std::endl;
     //std::cout << std::format( "color=\"{}\"", Color( ColorName::brown ) ) << std::endl;
--- include/color.hpp
+++ include/color.hpp
@@ -6,9 +6,55 @@
 
 #include <format>
 #include <string_view>
+#include <array>
 
 namespace svglib
 {
+	namespace conversion
+	{
+		constexpr char to_lower( char character )
+		{
+			if( character >= 65 and character <= 90 )
+				character += 97 - 65;
+
+			return character;
+		}
+
+		constexpr std::array hex_digits = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+
+		constexpr bool is_hex_digit( const char character )
+		{
+			return find( std::begin( hex_digits ), std::end( hex_digits ), to_lower( character ) ) != std::end( hex_digits );
+		}
+
+		constexpr const char* u8_to_hex_string( const std::uint8_t number )
+		{
+			const char left = static_cast< char >( number >> 4 );
+			const char right = static_cast< char >( number & 0x0f );
+
+			return new char[3] { hex_digits[left], hex_digits[right], '\0' };
+		}
+
+		constexpr std::uint8_t hex_char_to_u8( char character )
+		{
+			character = to_lower( character );
+
+			if( !is_hex_digit( character ) ) throw std::logic_error( "character must be a hexadecimal digit" );
+
+			return ( character >= 'a' and character <= 'f' ) ? character - 97 + 10 : character - 48;
+		}
+
+		constexpr std::uint8_t hex_string_to_u8( std::string_view hex )
+		{
+			if( hex.length() != 2 ) throw std::logic_error( "hex must be 2 characters long" );
+
+			const std::uint8_t first = hex_char_to_u8( hex[0] ) * 16;
+			const std::uint8_t second = hex_char_to_u8( hex[1] );
+
+			return first + second;
+		}
+	}
+
 	enum class ColorName : std::uint32_t
 	{
 		alice_blue = 0xF0F8FF,			/*!< rgb( 240, 248, 255 ) */
@@ -108,9 +154,25 @@ namespace svglib
 		return red_;
 	}
 
-	constexpr Color from_hex( std::string_view hex );
+	constexpr svglib::Color from_hex( std::string_view hex )
+	{
+		if( hex.length() != 6 ) throw std::logic_error( "hex must be 6 characters long" );
 
-	constexpr const char* to_hex( const Color& color );
+		return svglib::Color {
+			conversion::hex_string_to_u8( hex.substr( 0, 2 ) ),
+				conversion::hex_string_to_u8( hex.substr( 2, 2 ) ),
+				conversion::hex_string_to_u8( hex.substr( 4, 2 ) )
+		};
+	}
+
+	constexpr const char* to_hex( const svglib::Color& color )
+	{
+		const auto red = conversion::u8_to_hex_string( color.red() );
+		const auto green = conversion::u8_to_hex_string( color.green() );
+		const auto blue = conversion::u8_to_hex_string( color.blue() );
+
+		return new char[7] { red[0], red[1], green[0], green[1], blue[0], blue[1], '\0' };
+	}
 }
 
 template <>
