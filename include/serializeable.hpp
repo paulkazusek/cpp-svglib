bcf31ef6cde8a0685a6928124f49b54eb90e1994
paulkazusek <paul.kazusek@gmail.com>
2023-09-11 21:00:51 +0200

    add serializeable interface

---
+++ include/serializeable.hpp
@@ -0,0 +1,23 @@
+//
+//  serializeable.hpp
+
+#ifndef SERIALIZEABLE_HPP
+#define SERIALIZEABLE_HPP
+
+namespace svglib
+{
+	/**
+	 * \brief
+	 */
+	class Serializeable
+	{
+	public:
+		constexpr Serializeable() = default;
+
+		virtual constexpr ~Serializeable() = default;
+
+		[[nodiscard]] virtual constexpr std::string serialize() const = 0;
+	};
+}
+
+#endif
