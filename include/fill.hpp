05cb4cac5d5d6000bb69c4d58b79bcd08557aa49
paulkazusek <paul.kazusek@gmail.com>
2023-10-07 20:30:41 +0200

    add opacity to fill

--- include/fill.hpp
+++ include/fill.hpp
@@ -4,26 +4,37 @@
 #ifndef FILL_HPP
 #define FILL_HPP
 
+#include "opacity.hpp"
+
 #include <optional>
 
 namespace svglib
 {
 	class Color;
 
-	class Fill
+	class Fill final : public Serializeable
 	{
 	public:
 		constexpr Fill() = delete;
 
-		constexpr Fill( Color color, const std::optional<double>& opacity = std::nullopt )
+		constexpr Fill( Color color, std::optional<Opacity> opacity = std::nullopt )
 		//: color_ { std::move( color ) }
 		: color_ { color }
-		, opacity_ { opacity }
+		, opacity_ { std::move( opacity ) }
 		{}
+
+		[[nodiscard]] constexpr std::string serialize() const override
+		{
+			std::string opacity;
+			if( opacity_ )
+				opacity = " fill-" + opacity_.value().serialize();
+
+			return std::format( R"(fill="{}")", color_.serialize() ) + opacity;
+		}
 
 	private:
 		Color color_;
-		std::optional<double> opacity_;
+		std::optional<Opacity> opacity_;
 	};
 };
 
