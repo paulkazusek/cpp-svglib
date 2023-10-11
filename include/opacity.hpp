73ddb0a701ed558c037319328343423a31c42921
paulkazusek <paul.kazusek@gmail.com>
2023-10-11 20:51:47 +0200

    refactor width with unit

--- include/opacity.hpp
+++ include/opacity.hpp
@@ -4,20 +4,16 @@
 #ifndef OPACITY_HPP
 #define OPACITY_HPP
 
+#include "unit.hpp"
+
 namespace svglib
 {
-	enum class OpacityUnit : std::uint8_t
-	{
-		number,
-		percentage
-	};
-
 	class Opacity final : public Serializeable
 	{
 	public:
 		constexpr Opacity() = delete;
 
-		constexpr Opacity( const double& opacity, OpacityUnit unit = OpacityUnit::number )
+		explicit constexpr Opacity( const double& opacity, Unit unit = Unit::number )
 		: opacity_ { opacity }
 		, unit_ { unit }
 		{}
@@ -27,7 +23,7 @@ namespace svglib
 			if ( !isValid() )
 				return std::string{};
 
-			if ( unit_ == OpacityUnit::percentage )
+			if  ( unit_ == Unit::percentage )
 				return std::format( "opacity=\"{:}%\"", opacity_ );
 
 			return std::format( "opacity=\"{:.2f}\"", opacity_ );
@@ -36,14 +32,14 @@ namespace svglib
 	private:
 		[[nodiscard]] constexpr bool isValid() const
 		{
-			if ( unit_ == OpacityUnit::number )
+			if ( unit_ == Unit::number )
 				return opacity_ >= 0.0 and opacity_ <= 1.0 ? true : false;
 			
 			return opacity_ >= 0.0 and opacity_ <= 100.0 ? true : false;
 		}
 
 		double opacity_;
-		OpacityUnit unit_;
+		Unit unit_;
 	};
 };
 
