fc0d8b945c1c5336ac474b09a375f63d0e7804ce
paulkazusek <paul.kazusek@gmail.com>
2023-10-15 11:21:10 +0200

    refactor size with height und width type

--- include/size.hpp
+++ include/size.hpp
@@ -4,6 +4,9 @@
 #ifndef SIZE_HPP
 #define SIZE_HPP
 
+#include "height.hpp"
+#include "width.hpp"
+
 namespace svglib
 {
 	/**
@@ -14,9 +17,9 @@ namespace svglib
 	public:
 		constexpr Size() = delete;
 
-		constexpr Size( const double& width, const double& height )
-			: width_ { width }
-			, height_ { height }
+		constexpr Size( Width width, Height height )
+		: width_ { std::move( width ) }
+		, height_ { std::move( height ) }
 		{}
 
 		constexpr ~Size() = default;
@@ -32,29 +35,29 @@ namespace svglib
 		}
 
 	private:
-		double width_;
-		double height_;
+		Width width_;
+		Height height_;
 	};
 
-	constexpr Size screen_4k { 4096, 2160 };
-	constexpr Size screen_ultra_hd { 3840, 2160 };
-	constexpr Size screen_ultra_wide { 3840, 1080 };
-	constexpr Size screen_full_hd { 1920, 1080 };
-	constexpr Size screen_hd { 1280, 720 };
+	constexpr Size screen_4k { Width( 4096 ), Height( 2160 ) };
+	constexpr Size screen_ultra_hd { Width( 3840 ), Height( 2160 ) };
+	constexpr Size screen_ultra_wide { Width( 3840 ), Height( 1080 ) };
+	constexpr Size screen_full_hd { Width( 1920 ), Height( 1080 ) };
+	constexpr Size screen_hd { Width( 1280 ), Height( 720 ) };
 
 	/**
 	The ideal size for square posts is 1080px by 1080px at a 1:1 aspect ratio.
 	*/
-	constexpr Size instagram_post_size_square { 1080, 1080 };
+	constexpr Size instagram_post_size_square { Width( 1080 ), Height( 1080 ) };
 
 	/**
 	 The ideal size for landscape  posts is 1080px by 566px at a 1.91:1 aspect ratio.
 	 */
-	constexpr Size instagram_post_size_landscape { 1080, 566 };
+	constexpr Size instagram_post_size_landscape { Width( 1080 ), Height( 566 ) };
 
 	/**
 	 The ideal size for vertical posts is 1080px by 1350px at a 4:5 aspect ratio.
 	 */
-	constexpr Size instagram_post_size_vertical { 1080, 1350 };
+	constexpr Size instagram_post_size_vertical { Width( 1080 ), Height( 1350 ) };
 }
 #endif
