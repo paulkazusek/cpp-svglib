e5a053d3f34422cc162017c4f2c2e7d32085408c
paulkazusek <paul.kazusek@gmail.com>
2023-11-27 22:40:42 +0100

    refactoring

--- include/opacity.hpp
+++ include/opacity.hpp
@@ -1,8 +1,8 @@
 //
 //  opacity.hpp
 
-#ifndef OPACITY_HPP
-#define OPACITY_HPP
+#ifndef SVGLIB_OPACITY_HPP
+#define SVGLIB_OPACITY_HPP
 
 #include "unit.hpp"
 
@@ -19,9 +19,9 @@ namespace svglib
 		 * \param opacity decimal value ranging from 0.0 to 1.0 or percentage from 0.0% to 100.0% at which we want to set the opacity of the element 
 		 * \param unit decimal number or percentage value
 		 */
-		explicit constexpr Opacity( const double& opacity, const Unit unit = Unit::number )
-		: opacity_ { opacity }
-		, unit_ { unit }
+		explicit constexpr Opacity( const double& opacity, const UnitType unit = UnitType::user )
+			: opacity_ { opacity }
+			, unit_ { unit }
 		{}
 
 		[[nodiscard]] constexpr std::string serialize() const override
@@ -29,7 +29,7 @@ namespace svglib
 			if ( !isValid() )
 				return std::string{};
 
-			if  ( unit_ == Unit::percentage )
+			if  ( unit_ == UnitType::percentage )
 				return std::format( "opacity=\"{:}%\"", opacity_ );
 
 			return std::format( "opacity=\"{:.2f}\"", opacity_ );
@@ -38,14 +38,14 @@ namespace svglib
 	private:
 		[[nodiscard]] constexpr bool isValid() const
 		{
-			if ( unit_ == Unit::number )
+			if ( unit_ == UnitType::none )
 				return opacity_ >= 0.0 and opacity_ <= 1.0 ? true : false;
 			
 			return opacity_ >= 0.0 and opacity_ <= 100.0 ? true : false;
 		}
 
 		double opacity_;
-		Unit unit_;
+		UnitType unit_;
 	};
 }
 
